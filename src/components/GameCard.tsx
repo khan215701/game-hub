9aaf0d83d1817873ebbfa9036d89b6b0dc65cb9b
khan safiq <>
2023-11-29 12:04:44 +0530

    score badge

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Game } from "../hook/useGame";
-import { Card, Heading, Image } from "@chakra-ui/react";
+import { Card, HStack, Heading, Image } from "@chakra-ui/react";
 import GamePlatform from "./GamePlatform";
 import { Platform } from "./../hook/useGame";
+import GameBadge from "./GameBadge";
 
 interface props {
   game: Game;
@@ -13,9 +14,12 @@ const GameCard = ({ game }: props) => {
       <Card overflow="hidden" borderRadius={10}>
         <Image src={game.background_image} alt={game.name} />
         <Heading fontSize="2xl">{game.name}</Heading>
-        <GamePlatform
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent="space-between" padding="10px">
+          <GamePlatform
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <GameBadge score={game.metacritic} />
+        </HStack>
       </Card>
     </>
   );
